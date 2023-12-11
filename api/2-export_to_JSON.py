e3e72bbf662e6de471b9ca1580fd3b807fa7fec5
ChukwukaJ <117227123+ChukwukaJ@users.noreply.github.com>
2023-12-11 15:14:33 +0100

    Update 2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,28 +1,56 @@
 #!/usr/bin/python3
-""" Library to gather data from an API """
+"""
+Using what you did in the task #0,
+extend your Python script to export
+data in the JSON format.
 
-import json
-import requests
-import sys
+Requirements:
 
-""" Function to gather data from an API """
-
+Records all tasks that are owned by
+this employee Format must be: { "USER_ID":
+[{"task": "TASK_TITLE", "completed":
+TASK_COMPLETED_STATUS, "username": "USERNAME"},
+{"task": "TASK_TITLE", "completed":
+TASK_COMPLETED_STATUS, "username":
+"USERNAME"}, ... ]} File name must be:
+USER_ID.json
+"""
 if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    todo = "https://jsonplaceholder.typicode.com/todos?userId={}"
-    todo = todo.format(employee_id)
-
-    user_info = requests.request("GET", url).json()
-    todo_info = requests.request("GET", todo).json()
-
-    employee_username = user_info.get("username")
-
-    todos_info_sorted = [
-        dict(zip(["task", "completed", "username"],
-                 [task["title"], task["completed"], employee_username]))
-        for task in todo_info]
-
-    user_dict = {str(employee_id): todos_info_sorted}
-    with open(str(employee_id) + '.json', "w") as f:
-        f.write(json.dumps(user_dict))
+    import csv
+    import json
+    import requests
+    import sys
+    # using this url https://jsonplaceholder.typicode.com/todos/
+    # add a query string of userId = 2 using the requests module
+    url1 = "https://jsonplaceholder.typicode.com/todos"
+    url2 = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
+    payload = {"userId": sys.argv[1]}
+    # a sinle variable used to accept the response
+    # after request is made using the module
+    req_rep1 = requests.get(url1, params=payload)
+    req_rep2 = requests.get(url2)
+    req_rep1 = req_rep1.json()
+    req_rep2 = req_rep2.json()
+    # file name depends on id
+    filename2 = f"{sys.argv[1]}.json"
+    # create a dictionary having the key "userid" and
+    # the value should be an  empty list
+    user_data = {f'{req_rep2["id"]}': []}
+    # now get the keys of the user
+    user_keys = list(req_rep1[0].keys())
+    # now iterate over req_rep1 and req_rep2 to get
+    # to get a dictionary of task/title, completeted, username
+    # key value pairs
+    for data in req_rep1:
+        new_dict = {}
+        for key in user_keys:
+            if key != "title" and key != "completed":
+                continue
+            if key == "title":
+                new_dict["task"] = data[key]
+            else:
+                new_dict[key] = data[key]
+        new_dict["username"] = req_rep2["username"]
+        user_data.get(f"{sys.argv[1]}").append(new_dict)
+    with open(filename2, 'w', encoding='utf-8') as jsonfile:
+        json.dump(user_data, jsonfile)
