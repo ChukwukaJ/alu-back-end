bc9a8491438e8b8c75a831b7c42ee41036e885c7
ChukwukaJ <117227123+ChukwukaJ@users.noreply.github.com>
2023-12-11 15:13:51 +0100

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,27 +1,40 @@
 #!/usr/bin/python3
-""" Library to gather data from an API """
+"""
+Using what you did in the task #0,
+extend your Python script to export
+data in the CSV format.
 
-import requests
-import sys
+Requirements:
 
+Records all tasks that are owned by this employee
+Format must be:
+"USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+File name must be: USER_ID.csv
+"""
 if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    todo = "https://jsonplaceholder.typicode.com/todos?userId={}"
-    todo = todo.format(employee_id)
-
-    user_info = requests.request("GET", url).json()
-    todo_info = requests.request("GET", todo).json()
-
-    employee_name = user_info.get("name")
-    employee_username = user_info.get("username")
-    total_tasks = list(filter(lambda x: (x["completed"] is True), todo_info))
-    task_com = len(total_tasks)
-    total_task_done = len(todo_info)
-
-    with open(str(employee_id) + '.csv', "w") as f:
-        [f.write('"' + str(employee_id) + '",' +
-                 '"' + employee_username + '",' +
-                 '"' + str(task["completed"]) + '",' +
-                 '"' + task["title"] + '",' + "\n")
-         for task in todo_info]
+    import csv
+    import json
+    import requests
+    import sys
+    # using this url https://jsonplaceholder.typicode.com/todos/
+    # add a query string of userId = 2 using the requests module
+    url1 = "https://jsonplaceholder.typicode.com/todos"
+    url2 = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
+    payload = {"userId": sys.argv[1]}
+    # a sinle variable used to accept the response
+    # after request is made using the module
+    req_rep1 = requests.get(url1, params=payload)
+    req_rep2 = requests.get(url2)
+    req_rep1 = req_rep1.json()
+    req_rep2 = req_rep2.json()
+    # file name depends on id
+    filename = f"{sys.argv[1]}.csv"
+    with open(filename, 'w', newline='') as csvfile:
+        # create a csv writer object
+        data_writer = csv.writer(csvfile, delimiter=",", quotechar='"',
+                                 quoting=csv.QUOTE_ALL)
+        # iterate through the first request only and use the value of some key
+        # and use the username of the second request for every iteration
+        for data in req_rep1:
+            data_writer.writerow([data["userId"], req_rep2["username"],
+                                 data["completed"], data["title"]])
